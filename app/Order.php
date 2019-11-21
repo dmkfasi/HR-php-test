0bb4676d55892d09c805abea4320eee88335b5d8
Andrei Ivanov <andrei_ivanov3@epam.com>
2019-11-21 18:30:11 +0300

    getStatusName() method added

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    private $_line_item_amount = [0];
+
+    protected $fillable = [
+        'client_email',
+        'partner_id',
+        'status'
+    ];
+
+    public const orderStatus = [
+        0 => 'новый',
+        10 => 'подтвержден',
+        20 => 'завершен'
+    ];
+
+    public function products() {
+        return $this->belongsToMany(Product::class, 'order_products')
+            ->withPivot('price', 'quantity')
+            ->withTimestamps();
+    }
+
+    public function partner() {
+        return $this->belongsTo(Partner::class);
+    }
+
+    public function getStatusName() {
+        return self::orderStatus[$this->status];
+    }
+
+    public function getTotalAmount() {
+        $this->products->each(function ($p) {
+            $this->setLineItemAmount($p->pivot->price * $p->pivot->quantity);
+        });
+
+        $total = collect($this->_line_item_amount);
+
+        return $total->sum();
+    }    
+
+    public function setLineItemAmount($amount = 0) {
+        $this->_line_item_amount[] = $amount;
+    }
 }
