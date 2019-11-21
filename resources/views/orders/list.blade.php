fa78154067f2b46cb5fcaefe261c502c96f76c59
Andrei Ivanov <andrei_ivanov3@epam.com>
2019-11-21 18:38:37 +0300

    Order fetch status call added

---
+++ resources/views/orders/list.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    Список заказов
+
+    <table class="table-bordered m-3 p-3">
+        <th class="p-3 m-3">ид_заказа</th>
+        <th class="p-3 m-3">название_партнера</th>
+        <th class="p-3 m-3">стоимость_заказа</th>
+        <th class="p-3 m-3">наименование_состав_заказа</th>
+        <th class="p-3 m-3">статус_заказа</th>
+
+        <tbody>
+            @foreach ($orders as $order)
+            <tr>
+                <td class="p-3 m-3"><a href="{{ route('orders/edit', $order->id) }}" class="btn btn-primary">{{ $order->id }}</a></td>
+                <td class="p-3 m-3">{{ $order->partner->name }}</td>
+                <td class="p-3 m-3">{{ $order->getTotalAmount() }}</td>
+                <td class="p-3 m-3">{{ $order->products->implode('name', ', ') }}</td>
+                <td class="p-3 m-3">{{ $order->getStatusName() }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
