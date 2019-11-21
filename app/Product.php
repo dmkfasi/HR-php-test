c5b94afaa1e1b643418079ee30ad76c7d52fe691
Andrei Ivanov <andrei_ivanov3@epam.com>
2019-11-21 18:31:02 +0300

    Pivot table binding added

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function orders() {
+        return $this->belongsToMany(Order::class, 'order_products')
+            ->withPivot('price', 'quantity')
+            ->withTimestamps();
+    }
 }
