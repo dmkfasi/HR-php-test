bde9e7080231b0fd167b48ae2407a7170182972b
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-22 13:28:46 +0300

    Minor formatting

---
+++ app/Mail/OrderCompleted.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Order;
+
+class OrderCompleted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $order = null;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('orders.email-completed')
+                ->with('order', $this->order);
+    }
+}
