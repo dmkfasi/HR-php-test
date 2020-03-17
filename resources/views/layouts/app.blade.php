2057ac9a1e2448c5bc503471d4c0281aebf0b1fb
Andrei Ivanov <dmkfasi@gmail.com>
2020-03-17 22:29:31 +0300

    New look

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,40 +10,36 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light border border-dark">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="{{ url('/') }}" class="nav-link">Home</a></li>
                         @if (Route::has('weather'))
-                            <li><a href="{{ route('weather') }}">Погода</a></li>
+                        <li class="nav-item"><a href="{{ route('weather') }}" class="nav-link">Погода</a></li>
                         @endif
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
                         @if (Route::has('orders'))
-                            <li><a href="{{ route('orders') }}">Заказы</a></li>
+                        <li class="nav-item"><a href="{{ route('orders') }}" class="nav-link">Заказы</a></li>
                         @endif
                         @if (Route::has('products'))
-                            <li><a href="{{ route('products') }}">Товары</a></li>
+                        <li class="nav-item"><a href="{{ route('products') }}" class="nav-link">Товары</a></li>
                         @endif
                     </ul>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
 
         <main class="py-4">
             <div class="container">
--- resources/views/weather/yandex.blade.php
+++ resources/views/weather/yandex.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col"></div>
-    <div class="col">
-        <div class="card rounded bg-info p-2" style="width: 20em;">
+<div class="row align-items-center">
+    <div class="col-8">
+        <div class="card">
             <div class="text-center">
                 <p>Текущая температура в Брянске
                 <span class="badge">{{ $weather['fact']['temp'] }}&deg;C</span>
                 </a>
-              <p><a href="{{ $weather['info']['url'] }}" class="btn btn-primary" target="_blank">Полный прогноз здесь!</a></p>
+              <p><a href="{{ $weather['info']['url'] }}" class="btn btn-primary" target="_blank">Полный прогноз здесь</a></p>
             </div>
         </div>
     </div>
-    <div class="col"></div>
 </div>
 @endsection
